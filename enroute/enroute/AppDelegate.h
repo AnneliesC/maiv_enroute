b725b5545d7b3cd08d9ad2406af99229e2a17b1a
AnneliesC <annelies.clauwaert@telenet.be>
2014-06-06 13:45:20 +0200

    project setup

---
+++ enroute/enroute/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  enroute
+//
+//  Created by Annelies Clauwaert on 6/06/14.
+//  Copyright (c) 2014 Annelies Clauwaert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
